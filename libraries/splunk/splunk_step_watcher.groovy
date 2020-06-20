e16ed6e827815c1d938a5ddc8efc4dae7c4b1bb5
yeochinyi <yeochinyi@gmail.com>
2020-06-20 22:02:29 +0800

    ok

--- libraries/splunk/splunk_step_watcher.groovy
+++ libraries/splunk/splunk_step_watcher.groovy
@@ -3,7 +3,8 @@ void before(context){
     println "Splunk: running before the ${context.library} library's ${context.step} step"
 }
 
-@AfterStep
+//@AfterStep
+@AfterStep({ context.step in config.afterSteps })
 void after(context){
     println "Splunk: running after the ${context.library} library's ${context.step} step"
 }
