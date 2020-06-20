ad7ba49769f9e779bc2cc510751477775a64aa9c
yeochinyi <yeochinyi@gmail.com>
2020-06-20 21:58:34 +0800

    ok

--- libraries/splunk/splunk_pipeline_start.groovy
+++ libraries/splunk/splunk_pipeline_start.groovy
@@ -2,18 +2,3 @@
 void call(context){
     println "Splunk: beginning of the pipeline!"
 }
-
-@BeforeStep
-void before(context){
-    println "Splunk: running before the ${context.library} library's ${context.step} step"
-}
-
-@AfterStep
-void after(context){
-    println "Splunk: running after the ${context.library} library's ${context.step} step"
-}
-
-@CleanUp
-void call(context){
-    println "Splunk: end of the pipeline!"
-}
